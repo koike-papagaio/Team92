838750b50d340ff47c4b4c6ea0bf225ee35111f2
koike-papagaio <marukaziringo@gmail.com>
2022-06-07 19:00:47 +0900

    viewファイル作成

---
+++ resources/views/user/purchase_information.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>購入情報管理画面</title>
+</head>
+<body>
+    <!-- 表題 -->
+    <div></div>
+    <!-- 年月日指定 -->
+    <div></div>
+</body>
+</html>

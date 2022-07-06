4bf9d1ac7b5552ce56e738279c36307dc393bda7
“shunsukeshoda” <3633ss@gmail.com>
2022-07-06 15:09:34 +0900

    更新

--- resources/views/components/header.blade.php
+++ resources/views/components/header.blade.php
@@ -7,31 +7,33 @@
             <!-- 管理者画面 -->
             <div class="dropdown pe-5">
                     <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
-                        
+                        {{ Session::get('name') }}様
                     </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="#">商品管理/a></li>
-                        <li><a class="dropdown-item" href="#">商品登録</a></li>
-                        <li><a class="dropdown-item" href="#">購入情報管理</a></li>
-                        <li><a class="dropdown-item" href="#">ログアウト</a></li>
+                        <li><a class="dropdown-item" href="/product_management">商品管理/a></li>
+                        <li><a class="dropdown-item" href="/product_register">商品登録</a></li>
+                        <li><a class="dropdown-item" href="/purchase_information">購入情報管理</a></li>
+                        <li><a class="dropdown-item" href="{{ route('logout') }}">ログアウト</a></li>
                     </ul>
             </div>
         @else
             <!-- 会員画面 -->
-            
                 <div class="dropdown pe-5">
                  <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton1" data-bs-toggle="dropdown" aria-expanded="false">
                     {{ Session::get('name') }}様
                 </button>
                     <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-                        <li><a class="dropdown-item" href="#">会員編集</a></li>
-                        <li><a class="dropdown-item" href="#">購入履歴</a></li>
+                        <li><a class="dropdown-item" href="/member_edit/{id}">会員編集</a></li>
+                        <li><a class="dropdown-item" href="/buy_history">購入履歴</a></li>
                         <li><a class="dropdown-item" href="{{ route('logout') }}">ログアウト</a></li>
                     </ul>
                 </div>
 
                 <!-- カートアイコン -->
-                
+                <div class="cart-icon">
+                    <a href=""><img src="{{ asset('iconimg/cart.jpeg') }}" alt=""></a>
+                </div>
+            
         @endif
     @else
     <div class="dropdown pe-5">

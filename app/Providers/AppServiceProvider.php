bf4988a5b1f61305db8a868505152f1530e5343f
koike-papagaio <marukaziringo@gmail.com>
2022-07-11 16:15:42 +0900

    記述ミス

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,7 +24,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {        
-        if(\app::environment(['production'])){
+        if(\App::environment(['production'])){
             \URL::forseScheme('https');
         }
         Paginator::useBootstrap();
